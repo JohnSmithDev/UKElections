f7a80de1903aa2babbb346b0ceec2ca8307724b0
John Smith (aachen) <aachen@john-smith.me>
2019-03-23 15:31:13 +0000

    Fix missing imports; add Twitter/FB/OG card details; minor wording tweak

--- revoke_web.py
+++ revoke_web.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 """
 Web/HTML specific stuff for the revoke report
+
+This code is very clunky, but - for the time being at least - I want to be able
+to:
+* Not have any non-stdlib dependecies
+* Be able to generate a single standalone HTML file
 """
+
+import os
+import sys
+
+
+from misc import output_file
+
+
 EUREF_VOTES_BY_CONSTITUENCY_URL = 'https://commonslibrary.parliament.uk/' + \
                                   'parliament-and-elections/elections-elections/' + \
                                   'brexit-votes-by-constituency/'
@@ -25,9 +38,24 @@ def html_header(output_function, embed):
             output_function('''<link rel="stylesheet" type="text/css"
                   href="/static/table_colours.css" />\n''')
 
-        output_function('<title>%s</title></head>\n' % (title))
+        output_function('<title>%s</title>\n' % (title))
+        # Do we need both twitter: and og: metatags?  Fuck knows
+        output_function('''<meta name="twitter:card" content="summary_large_image"></meta>
+           <meta name="twitter:site" content="@JohnMMIX"></meta>
+           <meta name="twitter:creator" content="@JohnMMIX"></meta>
+           <meta name="twitter:url" content="https://john-smith-test.appspot.com/"></meta>
+           <meta name="twitter:description" content="Tabular real-time-ish analysis of the Revoke Article 50 petition's latest stats vs General Election 2017 and EU Referendum results"></meta>
+           <meta name="twitter:image" content="https://john-smith-test.appspot.com/static/screengrab.png"></meta>
 
-        output_function('<body>\n')
+
+           <meta name="og:url" content="https://john-smith-test.appspot.com/"></meta>
+           <meta name="og:title" content="Analysis of Revoke Article 50 petition"></meta>
+           <meta name="og:description" content="Tabular real-time-ish analysis of the Revoke Article 50 petition's latest stats vs General Election 2017 and EU Referendum results"></meta>
+           <meta name="og:image" content="https://john-smith-test.appspot.com/static/screengrab.png"></meta>
+        ''')
+
+
+        output_function('</head><body>\n')
         if embed:
             output_function('<script>\n')
             output_file(sys.stdout, os.path.join('static', 'sorttable.js'))
@@ -57,7 +85,7 @@ parliamentary constituency</p>''' % (petition_timestamp, signature_count,
 Table sorting (click on the headers) via <a href="https://www.kryogenix.org/code/browser/sorttable/">sorttable</a>.
 </p>''')
 
-        output_function('''<h2>%d constituencies currently have more petition signatures
+        output_function('''<h2>%d constituencies have more petition signatures
         than their GE2017 winning margin, of which %d voted in favour of leaving in
         the 2016 EU Referendum</h2>''' %
               (sig_above_margin, pro_leave_sig_above_margin))
