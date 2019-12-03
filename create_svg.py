e362a683443e24ff66392698394857de8bcb12b6
John Smith (aachen) <aachen@john-smith.me>
2019-12-03 23:15:18 +0000

    Very belated update to work with renamed includes, plus generates multiple output files.  JavaScript functionality is currently removed, but this was only a barebones implementation anyway

--- create_svg.py
+++ create_svg.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Output an SVG (stacked) bar chart of all constituencies
+"""
+
 
 import json
 import pdb
@@ -10,6 +14,7 @@ import sys
 from ec_data_reader import load_and_process_data, ADMIN_CSV, RESULTS_CSV
 from euref_data_reader import load_and_process_euref_data
 from misc import slugify
+from settings import *
 
 def output_file(output, filename):
     with open(filename) as copystream:
@@ -17,7 +22,23 @@ def output_file(output, filename):
         output.write(data)
 
 
-def output_svg(out, data):
+SORT_OPTIONS = {
+    # General constituency properties that (largely) don't change
+    'by_electorate': lambda z: z.constituency.electorate,
+    'by_region': lambda z: z.constituency.country_and_region,
+
+    # GE related
+    'by_turnout': lambda z: z.constituency.valid_votes,
+    'by_turnout_pc': lambda z: z.turnout_pc,
+    'by_winning_margin': lambda z: z.winning_margin,
+    'by_winner_votes': lambda z: z.winning_result.valid_votes,
+    'by_winning_margin_pc': lambda z: z.margin_pc,
+
+    # EURef related
+    'by_leave_pc': lambda z: z.constituency.euref.leave_pc
+    }
+
+def output_svg(out, data, sort_method='by_region'):
     out.write('''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
                 "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
@@ -34,13 +55,15 @@ def output_svg(out, data):
         fill: lightgrey;
       }
     ''')
-    with open('general_colours.css') as copystream:
+    with open(os.path.join(STATIC_DIR, 'party_and_region_colours.css')) as copystream:
         data = copystream.read()
         out.write(data)
     out.write(']]>\n  </style>\n')
 
 
     output_file(out, 'statictext.svg')
+
+    out.write(f'<text x="50" y="200">{sort_method}</text>\n')
 
 
     MARGIN = 10
@@ -58,7 +81,8 @@ def output_svg(out, data):
     # for i, conres in enumerate(sorted(election_data, key=lambda z: z.margin_pc)):
     # for i, conres in enumerate(sorted(election_data, key=lambda z: z.constituency.country_and_region)):
     # for i, conres in enumerate(sorted(election_data, key=lambda z: z.winning_party)):
-    for i, conres in enumerate(sorted(election_data, key=lambda z: z.constituency.euref.leave_pc)):
+    # for i, conres in enumerate(sorted(election_data, key=lambda z: z.constituency.euref.leave_pc)):
+    for i, conres in enumerate(sorted(election_data, key=SORT_OPTIONS[sort_method])):
         con = conres.constituency
         slugified_country = slugify(con.country)
         x_offset = MARGIN + (i * COLUMN_WIDTH)
@@ -67,7 +91,7 @@ def output_svg(out, data):
 
         out.write(f'<rect x="{x_offset}" y="{y_offset}" '
                   f'width="{COLUMN_WIDTH}" height="{height}" '
-                  f'class="constituency" '
+                  f'class="constituency total-electorate" '
                   f'title="{con.name}" />\n')
 
         country_y_pos = CONSTITUENCY_Y_OFFSET + MARGIN
@@ -97,11 +121,17 @@ def output_svg(out, data):
             party_y_offset -= party_height
             out.write(f'  <rect x="{x_offset}" y="{party_y_offset}" '
                       f'width="{COLUMN_WIDTH}" height="{party_height}" '
-                      f'class="result party-{slugified_party}" />\n')
+                      f'class="result constituency party-{slugified_party}" />\n')
 
 
+    # I believe election_bars.js is a precursor to constituency_details.js,
+    # that looks to be the case from looking at (a) the emacs ~ backup file and
+    # (b) the embedded content in temp/test.svg.
+    # Basically all it did/does was add a mouseover listener to
+    # rect.constituency elements that innerHTMLed the title attribute to
+    # an empty text element.
     out.write('<script type="text/ecmascript">\n<![CDATA[\n')
-    output_file(out, 'election_bars.js')
+    # qoutput_file(out, 'election_bars.js')
     out.write('\n]]>\n</script>')
     out.write('</svg>\n')
 
@@ -125,5 +155,8 @@ if __name__ == '__main__':
             pdb.set_trace()
     """
 
-    with open('test.svg', 'w') as output_stream:
-        output_svg(output_stream, election_data)
+
+    for sort_method in SORT_OPTIONS.keys():
+        output_filename = os.path.join('output', 'ge2017_bars_%s.svg' % (sort_method))
+        with open(output_filename, 'w') as output_stream:
+            output_svg(output_stream, election_data, sort_method=sort_method)
